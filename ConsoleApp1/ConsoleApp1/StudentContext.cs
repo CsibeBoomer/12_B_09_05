3ea8493bf39264b818fc9a567bf3e8487d37e47a
Tóthmihály Martin <b1totmar@vasvari.org>
2024-09-05 09:14:49 +0200

    .1

---
+++ ConsoleApp1/ConsoleApp1/StudentContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1;
+
+    public class BloggingContext : DbContext
+    {
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Subject> Subjects { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+            => options.UseSqlServer($"Data Source=");
+    }
+
